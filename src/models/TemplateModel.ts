82fd27aaff39b61e77ef1d292b0e2e8923eba3a4
Bobby <bobur0218programmer@gmail.com>
2024-12-02 22:20:16 +0500

    fix: potential models

--- src/models/TemplateModel.ts
+++ src/models/TemplateModel.ts
@@ -11,6 +11,7 @@ const TemplateSchema = new Schema({
   description: { type: String, required: true },
   createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // FK
   topic: { type: String, enum: Object.values(Topic), required: true },
+  isPublic: { type: Boolean, default: false },
 });
 
 export default model('Template', TemplateSchema);
--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -9,7 +9,7 @@ const UserSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   username: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
   passwordHash: { type: String, required: true },
   role: { type: String, enum: Object.values(UserRole), default: UserRole.USER },
   isBlocked: { type: Boolean, default: false },
