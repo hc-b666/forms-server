cb521cf46ad9755ec6c72c8b2427796f76071ed9
Bobby <bobur0218programmer@gmail.com>
2024-12-28 11:26:36 +0500

    delete: getErrorMessage.ts

--- dist/utils/getErrorMessage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getErrorMessage = getErrorMessage;
-function getErrorMessage(err) {
-    if (err instanceof Error)
-        return err.message;
-    return String(err);
-}
